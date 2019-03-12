963c7dd09840114e0dcac329ce1d23e2422e7ad0
xiejun <xiejun@Alamap.com>
2019-03-12 13:45:03 +0800

    add less file

---
+++ src/components/dsButton.vue
@@ -0,0 +1,36 @@
+/**
+* Created by xiejun on 2019/03/12.
+* ds button
+*/
+<template>
+  <div class="ds-button">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  name: '',
+  data() {
+    return {}
+  },
+  props: {},
+  computed: {},
+  created() {},
+  mounted() {},
+  watch: {},
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+@import '~@/styles/base/index.less';
+.ds-button {
+  background: @color-btn;
+  color: @color-txt;
+  margin-left: @padding;
+  .padding(10px, @padding);
+  .fs-btn();
+}
+</style>
